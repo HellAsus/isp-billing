19d4d1dab39936e31322db24b19b2c3757b41949
Me <asus.is.hell@gmail.com>
2020-09-03 14:22:33 +0300

    update

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Customers extends Model
+class Customer extends Model
 {
     use SoftDeletes;
     
--- app/Models/IpPool.php
+++ app/Models/IpPool.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class IpPools extends Model
+class IpPool extends Model
 {
     //
 }
